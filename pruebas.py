4524c54189454945eac9ea110d860745ff83c231
Diegocab <diegocabweb@gmail.com>
2022-10-17 14:41:46 -0500

    Pruebas curso python

---
+++ pruebas.py
@@ -0,0 +1,8 @@
+letras = ["a", "b", "c", "d"]
+letras.append("z")
+# Remueve solo la primera ocurrencia
+letras.remove("b")
+print(letras)
+dentro = "c" in letras
+# Imprime un booleano
+print(dentro)
